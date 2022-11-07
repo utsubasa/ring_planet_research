317c05448655c0581f86d5f3946a17825cdb03fc
utsubasa <umetani-tsubasa@ed.tmu.ac.jp>
2022-11-07 19:45:47 +0900

    不要なものを削除

--- ring_transit/research_umetani/tess_datapreprocess.py
+++ ring_transit/research_umetani/tess_datapreprocess.py
@@ -15,6 +15,7 @@ from astropy.io import ascii
 from astropy.table import vstack
 from scipy.stats import linregress, t
 import c_compile_ring
+
 warnings.filterwarnings("ignore")
 
 
@@ -298,11 +299,11 @@ def transit_fitting(
             if transitfit_params != None:
                 for p_name in p_names:
                     params[p_name].set(value=transitfit_params[p_name].value)
-                    if trial == "1st":
-                        pass
+                    if p_name != "t0":
+                        params[p_name].set(vary=False)
                     else:
-                        if p_name != "t0":
-                            params[p_name].set(vary=False)
+                        #t0の初期値だけ動かし、function evalsの少なくてエラーが計算できないことを回避
+                        params[p_name].set(value=np.random.uniform(-0.05, 0.05))
             if curvefit_params != None:
                 params.add_many(
                     curvefit_params["c0"],
@@ -657,6 +658,11 @@ def make_simulation_data():
     plt.savefig(f"{homedir}/fitting_result/figure/simulation_TOI495.01/before_process/each_lc/{i}.png")
     #plt.show()
     plt.close()
+    os.makedirs(
+                f"{homedir}/fitting_result/data/simulation_TOI495.01/before_process/each_lc",
+                exist_ok=True,
+            )
+    each_lc.write(f"{homedir}/fitting_result/data/simulation_TOI495.01/before_process/each_lc/{i}.csv")
 
     return each_lc
 
@@ -707,134 +713,6 @@ homedir = (
 
 oridf = pd.read_csv(f"{homedir}/exofop_tess_tois.csv")
 df = oridf[oridf["Planet SNR"] > 100]
-no_data_found_list = [
-    2645.01,
-    1518.01,
-    1567.01,
-    2626.01,
-    1905.01,
-    1355.01,
-    1498.01,
-    352.01,
-    2617.01,
-    1123.01,
-    2624.01,
-    2625.01,
-    2046.01,
-    2724.01,
-    2612.01,
-    2622.01,
-    1373.01,
-    628.01,
-    3109.01,
-    4283.01,
-    3604.01,
-    372.01,
-    2766.01,
-    1371.01,
-    2969.01,
-    3041.01,
-    3321.01,
-    3972.01,
-    1482.01,
-    2889.01,
-    2346.01,
-    2591.01,
-    2704.01,
-    4543.01,
-    1580.01,
-    2803.01,
-    1388.01,
-    1427.01,
-    2616.01,
-    3589.01,
-    2580.01,
-    1310.01,
-    2236.01,
-    1519.01,
-    3735.01,
-    3136.01,
-    2799.01,
-    4726.01,
-    3329.01,
-    1521.01,
-    2055.01,
-    2826.01,
-    1656.01,
-    4398.01,
-    4079.01,
-    2690.01,
-    2989.01,
-    3010.01,
-    2619.01,
-    2840.01,
-    3241.01,
-    4463.01,
-    2745.01,
-    1397.01,
-]  # short がないか、SPOCがないか
-no_perioddata_list = [
-    1134.01,
-    1897.01,
-    2423.01,
-    2666.01,
-    4465.01,
-]  # exofopの表にperiodの記載無し。1567.01,1656.01もperiodなかったがこちらはcadence=’short’のデータなし。
-no_signal_list = [2218.01]  # トランジットのsignalが無いか、ノイズに埋もれて見えない
-multiplanet_list = [1670.01, 201.01, 822.01]  # , 1130.01]
-startrend_list = [4381.01, 1135.01, 1025.01, 212.01, 1830.01, 2119.01, 224.01]
-flare_list = [212.01, 1779.01, 2119.01]
-two_epoch_list = [
-    671.01,
-    1963.01,
-    1283.01,
-    758.01,
-    1478.01,
-    3501.01,
-    845.01,
-    121.01,
-    1104.01,
-    811.01,
-    3492.01,
-    1312.01,
-    1861.01,
-    665.01,
-    224.01,
-    121.01,
-    2047.01,
-]
-ignore_list = [
-    1130.01,
-    224.01,
-    123.01,
-    1823.01,
-    1292.01,
-    2218.01,
-    964.01,
-]
-
-# 既に前処理したTOIの重複した前処理を回避するためのTOIのリスト
-done_list = os.listdir(
-    "/Users/u_tsubasa/Dropbox/ring_planet_research/folded_lc/figure/obs_t0"
-)
-done_list = [s for s in done_list if "TOI" in s]
-done_list = [s.lstrip("TOI") for s in done_list]
-done_list = [float(s.strip(".png")) for s in done_list]
-done_list = [float(s) for s in done_list]
-
-"""処理を行わないTOIを選択する"""
-df = df.set_index(["TOI"])
-# df = df.drop(index=done_list, errors="ignore")
-df = df.drop(index=no_data_found_list, errors="ignore")
-# df = df.drop(index=multiplanet_list, errors='ignore')
-df = df.drop(index=no_perioddata_list, errors="ignore")
-# df = df.drop(index=startrend_list, errors='ignore')
-df = df.drop(index=flare_list, errors="ignore")
-df = df.drop(index=two_epoch_list, errors="ignore")
-df = df.drop(index=no_signal_list, errors="ignore")
-# df = df.drop(index=ignore_list, errors="ignore")
-df = df.reset_index()
-
 df = df.sort_values("Planet SNR", ascending=False)
 df["TOI"] = df["TOI"].astype(str)
 TOIlist = df["TOI"]
@@ -860,43 +738,6 @@ for TOI in [495.01]:
     )  # translate to Rsun
     rs = param_df["Stellar Radius (R_Sun)"].values[0]
     rp_rs = rp / rs
-
-    """もしもduration, period, transit_timeどれかのパラメータがnanだったらそのTOIを記録して、処理はスキップする"""
-    if np.sum(np.isnan([duration, period, transit_time])) != 0:
-        # with open('nan3params_toi.dat', 'a') as f:
-        # f.write(f'{TOInumber}: {np.isnan([duration, period, transit_time])}\n')
-        continue
-
-    """lightkurveを用いてSPOCが作成した2min cadenceの全セクターのライトカーブをダウンロードする """
-    search_result = lk.search_lightcurve(
-        f"TOI{TOI}", mission="TESS", cadence="short", author="SPOC"
-    )
-    lc_collection = search_result.download_all()
-
-    """全てのライトカーブを結合し、fluxがNaNのデータ点は除去する"""
-    lc = lc_collection.stitch().remove_nans()  # initialize lc
-
-    """
-    lc.scatter()
-    plt.show()
-    import pdb;pdb.set_trace()
-    """
-
-    """多惑星系の場合、ターゲットのトランジットに影響があるかを判断する。
-    print('judging whether other planet transit is included in the data...')
-    time.sleep(1)
-    other_p_df = oridf[oridf['TIC ID'] == param_df['TIC ID'].values[0]]
-    if len(other_p_df.index) != 1:
-        lc = remove_others_transit(lc, oridf, param_df, other_p_df, TOI)
-    """
-
-    """ターゲットの惑星のtransit time listを作成"""
-    transit_time_list = np.append(
-        np.arange(transit_time, lc.time[-1].value, period),
-        np.arange(transit_time, lc.time[0].value, -period),
-    )
-    transit_time_list = np.unique(transit_time_list)
-    transit_time_list.sort()
 
     """各エポックで外れ値除去と多項式フィッティング"""
     
@@ -946,7 +787,7 @@ for TOI in [495.01]:
                     f"{homedir}/fitting_result/figure/simulation_TOI495.01/error_lc/under_90%_data/calc_t0/{TOInumber}/{TOInumber}_{str(i)}.png"
                 )
                 plt.close()
-            t0list.append(mid_transit_time)
+            #t0list.append(mid_transit_time)
             t0errlist.append(np.nan)
             continue
         else:
@@ -1011,7 +852,7 @@ for TOI in [495.01]:
         (1 / a_rs)
         * (np.sqrt(np.square(1 + rp_rs) - np.square(b)) / np.sin(inc))
     )
-    """transit fittingによって得たtransit time, period, durationを記録"""
+    """transit fittingによって得たtransit time, period, durationを記録
     obs_t0_idx = np.abs(np.asarray(t0list) - transit_time).argmin()
     os.makedirs(
         f"{homedir}/fitting_result/data/simulation_TOI495.01/folded_lc/modelresult/1stloop/{TOInumber}",
@@ -1027,7 +868,7 @@ for TOI in [495.01]:
         print(f"obs_transit_time_err[day]: {t0errlist[obs_t0_idx]}", file=f)
         print(f"obs_period[day]: {period}", file=f)
         print(f"obs_period_err[day]: {period_err}", file=f)
-    
+    """
     """fittingで得たtransit time listを反映"""
     obs_t0_list = t0list
     outliers = []
@@ -1057,7 +898,8 @@ for TOI in [495.01]:
             .remove_nans()
         )
         """
-        each_lc = make_simulation_data()
+        each_table = ascii.read(f"{homedir}/fitting_result/data/simulation_TOI495.01/before_process/each_lc/{i}.csv")
+        each_lc = lk.LightCurve(data=each_table)
         """解析中断条件を満たさないかチェック"""
         data_survival_rate = calc_data_survival_rate(each_lc, duration)
         if data_survival_rate < 0.9:
